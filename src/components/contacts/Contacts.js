78ae4abe20316748c29347282a4a2a082798b0bd
d.mcordova87@gmail.com <58273917+DavidC-CD@users.noreply.github.com>
2020-07-05 10:49:09 -0700

    cleanup and commentary

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useContext } from 'react';
 import Contact from './Contact';
 import { ContactContext } from '../../context/ContactState';
-import axios from 'axios';
 
 const Contacts = () => {
   const { contacts, getContacts } = useContext(ContactContext);
 
+  // Get contacts on render
   useEffect(() => {
     getContacts();
   }, [])
-  
+
   return (
     <>
       <h1 className="display-4 mb-2">
-        <span className="text-danger">Contact</span>
-                List
-              </h1>
+        <span className="text-danger">Contact</span>List
+      </h1>
+      {/* Check for contacts in state and map/create a contact component for each  */}
       {contacts && contacts.map(contact => (
         <Contact key={contact.id} contact={contact} />
       ))}
